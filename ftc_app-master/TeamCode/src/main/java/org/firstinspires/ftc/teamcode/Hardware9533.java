53a029e941aa57232c733eadd9a85f36fde57a85
ftc9533mm <ftc9533mm@gmail.com>
2016-11-05 08:57:32 -0400

    Create new repo for 2017 challenge

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware9533.java
@@ -0,0 +1,194 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import static java.lang.Math.abs;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a K9 robot.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left motor"
+ * Motor channel:  Right drive motor:        "right motor"
+ * Servo channel:  Servo to raise/lower arm: "arm"
+ * Servo channel:  Servo to open/close claw: "claw"
+ *
+ * Note: the configuration of the servos is such that:
+ *   As the arm servo approaches 0, the arm position moves up (away from the floor).
+ *   As the claw servo approaches 0, the claw opens up (drops the game element).
+ */
+public class Hardware9533
+{
+
+    public final static double ACCEL_RATE = 0.2;
+
+    private final static double MAX_SPEED = 0.7;
+
+    public boolean invertedDrive = false;
+
+    /* Public OpMode members. */
+    public DcMotor  leftMotor   = null;
+    public DcMotor  rightMotor  = null;
+
+    public DcMotor elevator = null;
+    public DcMotor intake = null;
+
+    public DcMotor shooterLeft = null;
+    public DcMotor shooterRight = null;
+
+
+
+
+    /* Local OpMode members. */
+    HardwareMap hwMap  = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public Hardware9533() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftMotor   = hwMap.dcMotor.get("left");
+        rightMotor  = hwMap.dcMotor.get("right");
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        intake = hwMap.dcMotor.get("ballGrabber");
+        elevator = hwMap.dcMotor.get("elevator");
+        elevator.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+        shooterLeft = hwMap.dcMotor.get("shooterLeft");
+        shooterRight = hwMap.dcMotor.get("shooterRight");
+        shooterRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        // Set all motors to zero power
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+        intake.setPower(0);
+        elevator.setPower(0);
+
+        shooterLeft.setPower(0);
+        shooterRight.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        elevator.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        shooterLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        shooterRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Define and initialize ALL installed servos.
+
+    }
+
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     */
+    public void waitForTick(long periodMs) {
+
+        long  remaining = periodMs - (long)period.milliseconds();
+
+        // sleep for the remaining portion of the regular cycle period.
+        if (remaining > 0) {
+            try {
+                Thread.sleep(remaining);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        // Reset the cycle clock for the next pass.
+        period.reset();
+    }
+
+    public void Stop(){
+        AccelerateMotor(this.leftMotor, 0);
+        AccelerateMotor(this.rightMotor, 0);
+    }
+
+    public void DriveRobot(double leftPower, double rightPower, Telemetry telemetry) {
+
+        double left = 0;
+        double right = 0;
+
+        if(this.invertedDrive) {
+
+            //reverse all the things
+            left = AccelerateMotor(this.rightMotor, -leftPower);
+            right = AccelerateMotor(this.leftMotor, -rightPower);
+
+        } else {
+
+            left = AccelerateMotor(this.leftMotor, leftPower);
+            right = AccelerateMotor(this.rightMotor, rightPower);
+
+        }
+
+
+
+        telemetry.addData("left",  "%.2f", left);
+        telemetry.addData("right", "%.2f", right);
+
+
+
+    }
+
+    public double AccelerateMotor(DcMotor motor, double targetPower) {
+        targetPower = Range.clip(targetPower, -MAX_SPEED, MAX_SPEED);
+
+        double power = this.accel(motor.getPower(), targetPower, ACCEL_RATE);
+        motor.setPower(power);
+        return power;
+
+    }
+
+
+    private double accel(double motorPower, double targetPower, double accelRate) {
+
+
+
+        if(accelRate < (abs(motorPower - targetPower))){
+            if(motorPower - targetPower < 0) {
+                return  (motorPower + accelRate);
+            } else {
+                return motorPower - accelRate;
+            }
+        } else {
+            return  targetPower;
+        }
+    }
+}
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateOpMode_Linear.java
@@ -0,0 +1,147 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="AutoBots: Vuforia", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+//@Disabled
+public class TemplateOpMode_Linear extends LinearOpMode {
+
+
+    private final boolean USE_VISION = true;
+    private final boolean TRACK_ROBOT_LOC = true;
+
+
+    /* Declare OpMode members. */
+    private final float MM_PER_INCH = 25.4f;
+    private final float ROBOT_WIDTH = 18*MM_PER_INCH;               // in mm
+    private final float FTC_FIELD_WIDTH = (12*12 - 2)*MM_PER_INCH;  // in mm
+    private final float TARGET_HEIGHT = 160.0f;                     // in mm
+    //
+    // If you copy our code, please register your own account and generate your own free license key at this site:
+    // https://developer.vuforia.com/license-manager
+    //
+    private final String VUFORIA_LICENSE_KEY =
+            "AdCwzDH/////AAAAGeDkDS3ukU9+lIXc19LMh+cKk29caNhOl8UqmZOymRGwVwT1ZN8uaPdE3Q+zceDu9AKNsqL9qLblSFV" +
+                    "/x8Y3jfOZdjMFs0CQSQOEyWv3xfJsdSmevXDQDQr+4KI31HY2YSf/KB/kyxfuRMk4Pi+vWS+oLl65o7sWPiyFgzoM74ENyb" +
+                    "j4FgteD/2b6B+UFuwkHWKBNpp18wrpkaiFfr/FCbRFcdWP5mrjlEZM6eOj171dybw97HPeZbGihnnxOeeUv075O7P167AVq" +
+                    "aiPy2eRK7OCubR32KXOqQKoyF6AXp+qu2cOTApXS5dqOOseEm+HE4eMF0S2Pld3i5AWBIR+JlPXDuc9LwoH2Q8iDwUK1+4g";
+
+    private final String TRACKABLES_FILE = "FTC_2016-17";
+
+    private FtcVuforia.Target[] targets =
+    {
+            //
+            // Blue alliance beacon 1.
+            //
+            new FtcVuforia.Target("wheels", 90.0f, 0.0f, 0.0f, 12.0f*MM_PER_INCH, FTC_FIELD_WIDTH/2.0f, TARGET_HEIGHT),
+            //
+            // Red alliance beacon 2.
+            //
+            new FtcVuforia.Target("tools", 90.0f, 0.0f, 90.0f, -FTC_FIELD_WIDTH/2.0f, 30.0f*MM_PER_INCH, TARGET_HEIGHT),
+            //
+            // Blue alliance beacon 2 location.
+            //
+            new FtcVuforia.Target("legos", 90.0f, 0.0f, 0.0f, -30.0f*MM_PER_INCH, FTC_FIELD_WIDTH/2.0f, TARGET_HEIGHT),
+            //
+            // Red alliance beacon 1 location.
+            //
+            new FtcVuforia.Target("gears", 90.0f, 0.0f, 90.0f, -FTC_FIELD_WIDTH/2.0f, -12.0f*MM_PER_INCH, TARGET_HEIGHT)
+    };
+
+    private FtcVuforia vuforia;
+    private boolean[] targetsFound = null;
+    Hardware9533 robot = new Hardware9533();
+    // DcMotor leftMotor = null;
+    // DcMotor rightMotor = null;
+
+
+
+
+    @Override
+    public void runOpMode() {
+        vuforia = new FtcVuforia(VUFORIA_LICENSE_KEY, com.qualcomm.ftcrobotcontroller.R.id.cameraMonitorViewId, VuforiaLocalizer.CameraDirection.BACK, TRACKABLES_FILE, targets.length);
+        //
+        // Phone location: Mounted center on the front of the robot with the back camera facing outward.
+        //
+        OpenGLMatrix phoneLocationOnRobot =
+                TRACK_ROBOT_LOC ? vuforia.locationMatrix(90.0f, 0.0f, 0.0f, 0.0f, ROBOT_WIDTH / 2.0f, 0.0f) : null;
+
+        vuforia.setTargets(targets, phoneLocationOnRobot);
+        targetsFound = new boolean[targets.length];
+        for (int i = 0; i < targetsFound.length; i++)
+        {
+            targetsFound[i] = false;
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            for (int i = 0; i < targets.length; i++) {
+                VuforiaTrackable target = vuforia.getTarget(i);
+                boolean visible = vuforia.isTargetVisible(target);
+
+
+                if(visible) {
+                    telemetry.addData(target.getName(), "visible");
+                }
+            }
+
+        }
+
+
+    }
+}
