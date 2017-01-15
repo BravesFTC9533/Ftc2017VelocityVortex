736c96c6195b3121bd37d6c2b0551da1bb11d3a9
kerfuffle <thepolarofbears@gmail.com>
2017-01-15 15:23:52 -0500

    New Beacon Color Detection

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
@@ -6,14 +6,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 import com.qualcomm.ftcrobotcontroller.R;
-import com.qualcomm.robotcore.eventloop.opmode.*;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.vuforia.HINT;
+import com.vuforia.PIXEL_FORMAT;
 import com.vuforia.Vuforia;
-
-import android.app.Activity;
 
 import org.firstinspires.ftc.robotcontroller.Util.Global;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
@@ -28,104 +25,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  * Created by Kerfuffle on 9/24/2016.
  */
 
-
-
-
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-//package org.firstinspires.ftc.robotcontroller.external.samples;
-
-import com.qualcomm.ftcrobotcontroller.R;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.RobotLog;
-
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.matrices.MatrixF;
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.teamcode.Util.ButtonRange;
-import org.lasarobotics.vision.android.Cameras;
-import org.lasarobotics.vision.ftc.resq.Beacon;
-import org.lasarobotics.vision.opmode.LinearVisionOpMode;
-import org.lasarobotics.vision.opmode.extensions.CameraControlExtension;
-import org.lasarobotics.vision.util.ScreenOrientation;
-import org.opencv.core.Size;
+import org.firstinspires.ftc.teamcode.Util.VortexUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import hallib.HalDashboard;
-
-
-/**
- * This OpMode illustrates the basics of using the Vuforia localizer to determine
- * positioning and orientation of robot on the FTC field.
- * The code is structured as a LinearOpMode
- *
- * Vuforia uses the phone's camera to inspect it's surroundings, and attempt to locate target images.
- *
- * When images are located, Vuforia is able to determine the position and orientation of the
- * image relative to the camera.  This sample code than combines that information with a
- * knowledge of where the target images are on the field, to determine the location of the camera.
- *
- * This example assumes a "diamond" field configuration where the red and blue alliance stations
- * are adjacent on the corner of the field furthest from the audience.
- * From the Audience perspective, the Red driver station is on the right.
- * The two vision target are located on the two walls closest to the audience, facing in.
- * The Stones are on the RED side of the field, and the Chips are on the Blue side.
- *
- * A final calculation then uses the location of the camera on the robot to determine the
- * robot's location and orientation on the field.
- *
- * @see VuforiaLocalizer
- * @see VuforiaTrackableDefaultListener
- * see  ftc_app/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained below.
- */
+import static org.firstinspires.ftc.teamcode.Util.VortexUtils.getImageFromFrame;
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Vuforia-Ryan", group = "vuf")
 //@Autonomous(name="Concept: Vuforia Navigation", group =sensorManager = (SensorManager) Activity.getSystemService(SENSOR_SERVICE);"Concept")
@@ -140,32 +43,6 @@ public class VuforiaOp extends MMOpMode_Linear{ //extends MMOpMode_Linear{
     private final int RED = 0, BLUE = 1;
     private String leftBeacon, rightBeacon;
     VuforiaTrackables beacons;
-
-
-    /*private void detectBeacon() throws InterruptedException
-    {
-        waitForVisionStart();
-
-        this.setCamera(Cameras.SECONDARY);
-        this.setFrameSize(new Size(900, 900));
-
-        enableExtension(Extensions.BEACON);         //Beacon detection
-        enableExtension(Extensions.ROTATION);       //Automatic screen rotation correction
-        enableExtension(Extensions.CAMERA_CONTROL); //Manual camera control
-
-        beacon.setAnalysisMethod(Beacon.AnalysisMethod.FAST);
-        beacon.setColorToleranceRed(0);
-        beacon.setColorToleranceBlue(0);
-
-        rotation.setIsUsingSecondaryCamera(false);
-        rotation.disableAutoRotate();
-        rotation.setActivityOrientationFixed(ScreenOrientation.PORTRAIT);
-
-        cameraControl.setColorTemperature(CameraControlExtension.ColorTemperature.AUTO);
-        cameraControl.setAutoExposureCompensation();
-    }*/
-
-
 
     private void initVuforia() {
 
@@ -231,7 +108,25 @@ public class VuforiaOp extends MMOpMode_Linear{ //extends MMOpMode_Linear{
         DONE
     }
 
+    public int getBeaconColor(VuforiaTrackableDefaultListener listener)
+    {
+        int color = -1;
 
+        try {
+            color = VortexUtils.waitForBeaconConfig(getImageFromFrame(vuforia.getFrameQueue().take(), PIXEL_FORMAT.RGB565), listener, vuforia.getCameraCalibration(), 5000);
+        } catch (InterruptedException e){}
+
+        if (color == VortexUtils.BEACON_BLUE_RED)
+        {
+            robot.dashboard.displayPrintf(15, "Blue on left, Red on right");
+        }
+        else if (color == VortexUtils.BEACON_RED_BLUE)
+        {
+            robot.dashboard.displayPrintf(15, "Red on left, Blue on right");
+        }
+
+        return color;
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -256,6 +151,9 @@ public class VuforiaOp extends MMOpMode_Linear{ //extends MMOpMode_Linear{
         boolean move = true;
 
         runtime.reset();
+
+        Global.opModeActive = opModeIsActive();
+
         while (opModeIsActive()) {
 
 
@@ -279,6 +177,8 @@ public class VuforiaOp extends MMOpMode_Linear{ //extends MMOpMode_Linear{
                 }
 
             }
+
+
 
 
             switch (state){
